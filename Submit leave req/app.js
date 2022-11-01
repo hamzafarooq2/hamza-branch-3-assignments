fbf1665a8a85a16d071e1b88ba92877c82fce6e9
hamzafarooq2 <hamzaf0000@gmail.com>
2022-11-01 13:20:29 +0500

    submit leave request

---
+++ Submit leave req/app.js
@@ -0,0 +1,71 @@
+function check1(){
+    document.getElementById("rad1").checked = true;
+    const background1 = document.getElementById("b1");
+    const background2 = document.getElementById("b2");
+    const background3 = document.getElementById("b3");
+    const background4 = document.getElementById("b4");
+
+    background1.style.backgroundColor = "#AFEEEE";
+    background2.style.backgroundColor = "white";
+    background3.style.backgroundColor = "white";
+    background4.style.backgroundColor = "white";
+
+    background1.style.borderColor = "#008B8B";
+    background2.style.borderColor = "#e9e5e8";
+    background3.style.borderColor = "#e9e5e8";
+    background4.style.borderColor = "#e9e5e8";
+
+}
+function check2(){
+    document.getElementById("rad2").checked = true;
+    const background1 = document.getElementById("b1");
+    const background2 = document.getElementById("b2");
+    const background3 = document.getElementById("b3");
+    const background4 = document.getElementById("b4");
+
+    background1.style.backgroundColor = "white";
+    background2.style.backgroundColor = "#AFEEEE";
+    background3.style.backgroundColor = "white";
+    background4.style.backgroundColor = "white";
+
+    background1.style.borderColor = "#e9e5e8";
+    background2.style.borderColor = "#008B8B";
+    background3.style.borderColor = "#e9e5e8";
+    background4.style.borderColor = "#e9e5e8";
+   
+
+}
+function check3(){
+    document.getElementById("rad3").checked = true;
+    const background1 = document.getElementById("b1");
+    const background2 = document.getElementById("b2");
+    const background3 = document.getElementById("b3");
+    const background4 = document.getElementById("b4");
+
+    background1.style.backgroundColor = "white";
+    background2.style.backgroundColor = "white";
+    background3.style.backgroundColor = "#AFEEEE";
+    background4.style.backgroundColor = "white";
+
+    background1.style.borderColor = "#e9e5e8";
+    background2.style.borderColor = "#e9e5e8";
+    background3.style.borderColor = "#008B8B";
+    background4.style.borderColor = "#e9e5e8";
+}
+function check4(){
+    document.getElementById("rad4").checked = true;
+    const background1 = document.getElementById("b1");
+    const background2 = document.getElementById("b2");
+    const background3 = document.getElementById("b3");
+    const background4 = document.getElementById("b4");
+
+    background1.style.backgroundColor = "white";
+    background2.style.backgroundColor = "white";
+    background3.style.backgroundColor = "white";
+    background4.style.backgroundColor = "#AFEEEE";
+
+    background1.style.borderColor = "#e9e5e8";
+    background2.style.borderColor = "#e9e5e8";
+    background3.style.borderColor = "#e9e5e8";
+    background4.style.borderColor = "#008B8B";
+}
