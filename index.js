be1bda1e35e9628cc0cc82d38042114a2846795b
hamzafarooq2 <hamzaf0000@gmail.com>
2022-11-08 18:20:47 +0500

    Filter all, casual and sick data

---
+++ index.js
@@ -0,0 +1,41 @@
+function allFunction() {
+  const box_1 = document.querySelector(".box_1");
+  box_1.style.display = "contents";
+
+  const box_2 = document.querySelector(".box_2");
+  box_2.style.display = "contents";
+
+  const box_3 = document.querySelector(".box_3");
+  box_3.style.display = "contents";
+
+  const box_4 = document.querySelector(".box_4");
+  box_4.style.display = "contents";
+}
+
+function casualFunction() {
+  const box_1 = document.querySelector(".box_1");
+  box_1.style.display = "contents";
+
+  const box_3 = document.querySelector(".box_3");
+  box_3.style.display = "contents";
+
+  const box_2 = document.querySelector(".box_2");
+  box_2.style.display = "none";
+
+  const box_4 = document.querySelector(".box_4");
+  box_4.style.display = "none";
+}
+
+function sickFunction() {
+  const box_1 = document.querySelector(".box_1");
+  box_1.style.display = "none";
+
+  const box_3 = document.querySelector(".box_3");
+  box_3.style.display = "none";
+
+  const box_2 = document.querySelector(".box_2");
+  box_2.style.display = "contents";
+
+  const box_4 = document.querySelector(".box_4");
+  box_4.style.display = "contents";
+}
